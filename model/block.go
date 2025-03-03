9183836437ac001c217ca8c22817cc45b9b5ee63
lifeng <lifeng@novatech.team>
2025-03-03 15:43:14 +0800

    增加数据库 迁移

--- model/block.go
+++ model/block.go
@@ -7,10 +7,10 @@ import (
 
 type BlockModel struct {
 	ID           int            `gorm:"column:id;type:integer;primaryKey"`
-	Url          string         `gorm:"column:url;type:varchar(100);comment:'图片minio存储地址'"`
+	Url          string         `gorm:"column:url;type:varchar(200);comment:'图片minio存储地址'"`
 	Title        string         `gorm:"column:title;type:varchar(30);not null;comment:'模型名称'"`
 	ModelType    int            `gorm:"column:model_type;type:integer;not null;comment:'模型类别对模型分类表主键id'"`
-	ModelFile    string         `gorm:"column:model_file;type:varchar(100);comment:'图片minio存储地址'"`
+	ModelFile    string         `gorm:"column:model_file;type:varchar(200);comment:'图片minio存储地址'"`
 	BucketName   string         `gorm:"column:bucket_name;type:varchar(20);comment:'图片minio存储桶名'"`
 	Hot          int            `gorm:"column:hot;type:integer;default:0;comment:'热度'"`
 	Tags         pq.StringArray `gorm:"column:tags;type:varchar(20)[];comment:'模型标签'"`
