f44519f7adab9b853dc86f100d8b0a5915604e43
zimu <eli_zimu@163.COM>
2025-03-03 23:04:08 +0800

    修改启动和关闭服务

--- main.go
+++ main.go
@@ -36,7 +36,6 @@ func main() {
 		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			log.Fatalf("listen: %s\n", err)
 		}
-		fmt.Println("✅ swagger:", address, "/swagger/index.html")
 	}()
 	// 等待中断信号以优雅地关闭服务器（设置 5 秒的超时时间）
 	quit := make(chan os.Signal, 1)
